dc794c058a3982b30ce6216c7f6d39ac9761f95b
Konstantin Botalov <konstantin@botalov.pro>
2023-11-05 01:32:57 +0300

    change order of urls

--- BigHouzzz/urls.py
+++ BigHouzzz/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
     path('cars/', include('cars.urls')),
     # TODO: path('flat/', include('flats.urls')),
+    path('', include('core.urls')),
 ]
