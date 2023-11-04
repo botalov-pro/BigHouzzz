c0b3458542586b1d5838afcc0306d861dd38b886
Konstantin Botalov <konstantin@botalov.pro>
2023-11-05 01:35:32 +0300

    add templates files for about and contacts

--- core/views.py
+++ core/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Главная страница</h1>')
+    # FIXME: удалить return HttpResponse('<h1>Главная страница</h1>')
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contacts(request):
+    return render(request, 'core/contacts.html')
