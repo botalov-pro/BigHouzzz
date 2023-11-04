b9c315e92a73a4b6fd9313fa8060ca40b2072744
Konstantin Botalov <konstantin@botalov.pro>
2023-11-05 01:35:01 +0300

    add template file for cars_index

--- cars/views.py
+++ cars/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cars_index(request):
-    return HttpResponse('<h1>Машинки!</h1>')
+    # FIXME: удалить return HttpResponse('<h1>Машинки!</h1>')
+    return render(request, 'cars/index.html')
