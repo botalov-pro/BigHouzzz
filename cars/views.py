ff88fb33050792a31874273c4e2dfb56698aa4ca
Konstantin Botalov <konstantin@botalov.pro>
2023-11-06 20:52:07 +0300

    remove unusefull libraries

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def cars_index(request):
-    # FIXME: удалить return HttpResponse('<h1>Машинки!</h1>')
     return render(request, 'cars/index.html')
