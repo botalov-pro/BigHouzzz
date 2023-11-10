c295bb4ee2b508903ceae7a1715b6bb658501869
Konstantin Botalov <konstantin@botalov.pro>
2023-11-10 13:26:39 +0300

    Change index.html for using with Django Templates

--- core/views.py
+++ core/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    # FIXME: удалить return HttpResponse('<h1>Главная страница</h1>')
-    return render(request, 'core/index.html')
+    context = {
+        'Title': 'BigHouzzz',
+        'Header1': 'Главная страница!',
+    }
+    return render(request, 'core/index.html', context)
 
 def about(request):
     return render(request, 'core/about.html')
