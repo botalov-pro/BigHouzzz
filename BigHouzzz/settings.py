8ef8e3f32ec95b896e26113318b0d996640ca2ea
Konstantin Botalov <konstantin@botalov.pro>
2023-11-12 17:20:04 +0300

    Change auth_user_model

--- BigHouzzz/settings.py
+++ BigHouzzz/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -149,3 +149,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMPTY_VALUE_DISPLAY = '-пусто-'
+
+AUTH_USER_MODEL = 'users.User'
