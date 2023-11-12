7d2d0df4795dc4bd39017f53b25c407cb06e02a3
Konstantin Botalov <konstantin@botalov.pro>
2023-11-12 16:42:57 +0300

    Add app_name for users app

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('login/', views.users_login, name='users_login'),
     path('signup/', views.users_signup, name='users_signup'),
