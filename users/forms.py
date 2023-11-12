2764ff923b8d11b8967535c6c674ec164569792d
Konstantin Botalov <konstantin@botalov.pro>
2023-11-13 02:59:20 +0300

    Imported modules for auth forms

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
 from users.models import User
 
