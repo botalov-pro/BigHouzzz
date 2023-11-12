7f154e30febd495336319dd26d66267f2c7100b2
Konstantin Botalov <konstantin@botalov.pro>
2023-11-12 16:40:30 +0300

    Add app_name for cars

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+
+app_name = 'core'
+
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('contacts/', views.contacts, name='contacts'),
