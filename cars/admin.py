d2e803977381990ca9d2765ac7aceb09fe3e7407
Konstantin Botalov <konstantin@botalov.pro>
2023-11-12 12:42:17 +0300

    Add in admin for cars display for empty values

--- cars/admin.py
+++ cars/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from BigHouzzz.settings import EMPTY_VALUE_DISPLAY
 from .models import Vehicle
 
 @admin.register(Vehicle)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['Regnum', 'Model', 'IsRegnumAlien', 'Created', 'Updated']
     list_filter = ['Created', 'Updated']
+    empty_value_display = EMPTY_VALUE_DISPLAY
