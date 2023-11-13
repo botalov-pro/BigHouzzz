34be1617da10a142207992541979a4900f362ade
Konstantin Botalov <konstantin@botalov.pro>
2023-11-14 01:06:18 +0300

    Add car_detail functional

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'cars'
 
 urlpatterns = [
+    path('<int:car_id>/', views.car_detail, name='car_detail'),
     path('', views.cars_index, name='cars_index'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Vehicle
+
 
 def cars_index(request):
     return render(request, 'cars/index.html')
+def car_detail(request, car_id):
+    vehicle = get_object_or_404(Vehicle, pk=car_id)
+    context = {
+        'vehicle': vehicle,
+    }
+    return render(request, 'cars/car_detail.html', context)
