082a351a8fd1d5d7aab9249dd14895ed2554d624
Konstantin Botalov <konstantin@botalov.pro>
2023-11-14 01:10:27 +0300

    Fix wrong label for Model, small edit

--- cars/models.py
+++ cars/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Vehicle(models.Model):
     Regnum = models.CharField('Регистрационный знак', max_length=10, db_index=True)
     IsRegnumAlien = models.BooleanField('Иностранный?', default=False)
-    Model = models.CharField('Регистрационный знак', max_length=200)
+    Model = models.CharField('Модель', max_length=200)
     Color = models.CharField('Цвет', max_length=200, default=None)
     Created = models.DateTimeField('Дата создания', auto_now_add=True)
     Updated = models.DateTimeField('Дата обновления', auto_now=True)
