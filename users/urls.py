12519a90e50045348344d742e9a0dc81908a5ac1
Konstantin Botalov <konstantin@botalov.pro>
2023-11-13 03:07:21 +0300

    Add logout path

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
 ]
