2aaa7fe6ef94c178fb22840957078445e25de4cc
Konstantin Botalov <konstantin@botalov.pro>
2023-11-13 03:03:00 +0300

    Fix problem with static files

--- BigHouzzz/urls.py
+++ BigHouzzz/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -25,6 +28,13 @@ urlpatterns = [
     path('flats/', include('flats.urls')),
     path('', include('core.urls')),
 ]
+
+# Путь для статических файлов
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
