f4055e5a343a4911b23ee271b89e04a7d33522d5
Konstantin Botalov <konstantin@botalov.pro>
2023-11-14 01:11:23 +0300

    Redesign view for car_index

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,14 @@ from .models import Vehicle
 
 
 def cars_index(request):
-    return render(request, 'cars/index.html')
+    num_vehicles = Vehicle.objects.count()
+    vehicles = Vehicle.objects.all()
+    context = {
+        'num_vehicles': num_vehicles,
+        'vehicles': vehicles,
+    }
+    return render(request, 'cars/index.html', context)
+
 def car_detail(request, car_id):
     vehicle = get_object_or_404(Vehicle, pk=car_id)
     context = {
