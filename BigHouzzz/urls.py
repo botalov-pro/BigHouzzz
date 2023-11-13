2d5c2bafed4a73c46fee10f49a51632122ee9b84
Konstantin Botalov <konstantin@botalov.pro>
2023-11-14 01:10:48 +0300

    Small edit

--- BigHouzzz/urls.py
+++ BigHouzzz/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('auth/', include('users.urls')),
