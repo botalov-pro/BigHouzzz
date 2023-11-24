6258182f754b1397574f77d8ed007669157844ee
Konstantin Botalov <konstantin@botalov.pro>
2023-11-24 18:13:10 +0300

    Add new line

--- cars/admin.py
+++ cars/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from BigHouzzz.settings import EMPTY_VALUE_DISPLAY
 from .models import Vehicle
 
+
 @admin.register(Vehicle)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['Regnum', 'Model', 'IsRegnumAlien', 'Created', 'Updated']
