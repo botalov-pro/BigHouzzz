a9d70dd38099dae1acb017386beb27bf47581206
Konstantin Botalov <konstantin@botalov.pro>
2023-11-26 15:30:24 +0300

    Add Colors model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from BigHouzzz.settings import EMPTY_VALUE_DISPLAY
+from .models import Colors
 
-# Register your models here.
+
+@admin.register(Colors)
+class ColorsAdmin(admin.ModelAdmin):
+    """ Цвета - Администрирование """
+    fields = []
+    list_display = ['name', 'is_active']
+    empty_value_display = EMPTY_VALUE_DISPLAY
