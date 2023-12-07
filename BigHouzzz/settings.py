15789bd7db9642c8995e1a3ccb77a78c1c1a7f17
Konstantin Botalov <konstantin@botalov.pro>
2023-12-07 23:19:29 +0300

    Changed INSTALLED_APPS: correct name for bootstrap5

--- BigHouzzz/settings.py
+++ BigHouzzz/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django_bootstrap5',
     'debug_toolbar',
     'users',
     'flats',
