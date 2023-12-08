980488d28f0a08c32775ceea0d519404824ea6d7
Konstantin Botalov <konstantin@botalov.pro>
2023-12-08 09:34:29 +0300

    Add: docstrings to views.py

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib import auth, messages
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 def login(request):
+    """ Вход пользователя """
     if request.method == 'POST':  # Если POST-запрос на получение данных с формы
         login_form = UserLoginForm(data=request.POST)
         if login_form.is_valid():
@@ -21,6 +22,7 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def signup(request):
+    """ Регистрация нового пользователя """
     if request.method == 'POST':  # Если POST-запрос на получение данных с формы
         registration_form = UserRegistrationForm(data=request.POST)
         if registration_form.is_valid():
@@ -36,6 +38,7 @@ def signup(request):
 
 
 def profile(request):
+    """ Профиль пользователя """
     if request.method == 'POST':  # Если POST-запрос на получение данных с формы
         profile_form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
         if profile_form.is_valid():
@@ -50,5 +53,6 @@ def profile(request):
 
 
 def logout(request):
+    """ Выход пользователя """
     auth.logout(request)
     return redirect('core:index')  # перенаправить на Главную
