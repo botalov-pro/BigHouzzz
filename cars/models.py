5fdd249726ab9d37397347c07f7e475b8cc8058f
Konstantin Botalov <konstantin@botalov.pro>
2024-01-03 19:52:31 +0300

    Add new empty class Act

--- cars/models.py
+++ cars/models.py
@@ -107,3 +107,47 @@ class Vehicle(models.Model):
 
     def __str__(self):
         return self.regnum
+
+class Act(models.Model):
+    """ Поведение """
+    pass
+    #
+    # # CHOICES
+    #
+    # # DATABASE FIELDS
+    # user = models.ForeignKey(
+    #     User,
+    #     verbose_name='Пользователь',
+    #     on_delete=models.PROTECT,
+    #     blank=True,
+    #     null=True,
+    # )
+    # rating = models.CharField(
+    #     'Оценка',
+    #     max_length=1,
+    #     choices=RATING_CHOICES,
+    #     blank=True,
+    # )
+    # text = models.TextField(
+    #     'Текст отзыва',
+    #     max_length=250,
+    #     blank=True,
+    # )
+    # is_active = models.BooleanField(
+    #     'Активный',
+    #     default=True
+    # )
+    # created = models.DateTimeField(
+    #     'Дата создания',
+    #     auto_now_add=True
+    # )
+    #
+    # # META CLASS
+    # class Meta:
+    #     verbose_name = 'Отзыв'
+    #     verbose_name_plural = 'Отзывы'
+    #     ordering = ('created',)
+    #
+    # # TO STRING METHOD
+    # # def __str__(self):
+    # #    return self.id
