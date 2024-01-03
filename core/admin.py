32914cb419e2bdf3a62aff5a5ac8e5ad76363490
Konstantin Botalov <konstantin@botalov.pro>
2024-01-03 19:50:42 +0300

    Add new class Review to admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from BigHouzzz.settings import EMPTY_VALUE_DISPLAY
-from .models import Colors
+from .models import Colors, Review
 
 
 @admin.register(Colors)
@@ -12,4 +12,24 @@ class ColorsAdmin(admin.ModelAdmin):
     list_filter = ['is_active',]
     search_fields = ['name',]
     ordering = ['name',]
+    empty_value_display = EMPTY_VALUE_DISPLAY
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """ Отзывы - Администрирование """
+    fields = [
+        'user',
+        'rating',
+        'text',
+        'is_active',
+        'created',
+    ]
+    list_display = ['id', 'user', 'text', 'rating', 'created', 'is_active',]
+    list_display_links = ['id',]
+    list_filter = ['is_active', 'created', 'rating',]
+    readonly_fields = [
+        'created',
+    ]
+    search_fields = ['text',]
+    ordering = ['-created',]
     empty_value_display = EMPTY_VALUE_DISPLAY
